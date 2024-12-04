642b4d008e21ab99833e441e15d8a79c3bd289f7
david-kasprak <kasprakdavid@gmail.com>
2024-12-04 16:45:49 +0200

    HW-1 (DONE)

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log('main js')
+const http = require('node:http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    const url = req.url;
+    if (url === '/') {
+        res.end(JSON.stringify({
+            data: 'Home Page'
+        }));
+    }
+    if (url === '/about') {
+        res.end(JSON.stringify({
+            data: 'About Page'
+        }));
+    }
+    if (url === '/contact') {
+        res.end(JSON.stringify({
+            data: 'Contact Page'
+        }));
+    }
+
+
+});
+
+server.listen(8000);
