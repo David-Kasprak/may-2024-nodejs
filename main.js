e405d6d70766b3dca558910aaa43d66f5b728797
david-kasprak <kasprakdavid@gmail.com>
2024-12-11 12:38:01 +0200

    HW-3 (in progress)

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
+const express = require('express');
 const dotenv = require('dotenv')
+const {read, write} = require('./fs.service');
 dotenv.config()
 
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+// Reading users
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+// Creating new user
+app.post('/users', (req, res) => {
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }
+    users.push(newUser)
+    res.status(201).json(newUser);
+});
+// Reading user by id todo
+app.get()
+
+// Deleting user by id
 
 const port = process.env.PORT;
-console.log('main js')
