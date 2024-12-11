c64ebbaa5232bd01cd3fb75a041cdc716dbabb4c
david-kasprak <kasprakdavid@gmail.com>
2024-12-11 19:40:01 +0200

    HW-3 (still in progress...)

--- main.js
+++ main.js
@@ -7,25 +7,62 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
 // Reading users
-app.get('/users', (req, res) => {
-    res.json(users);
+app.get('/users', async (req, res) => {
+    try {
+        const users = await read();
+        res.json(users);
+    }
+    catch (e) {
+        res.status(500).json(e.message);
+    }
 });
 // Creating new user
-app.post('/users', (req, res) => {
-    const newUser = {
-        id: users.length + 1,
+app.post('/users', async (req, res) => {
+    try {
+        const users = await read();
+        const newUser = {
+        id: users.length ? users [users.length - 1].id + 1 : 1,
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
-    }
-    users.push(newUser)
-    res.status(201).json(newUser);
+        }
+        users.push(newUser);
+        await write(users);
+        res.status(201).json(newUser);
+        }
+    catch (e) {
+        res.status(500).json(e.message);
+        }
 });
-// Reading user by id todo
-app.get()
-
+// Reading user by id
+app.get('/users/:userId', async (req, res) => {
+    try {
+        const users = await read();
+        const user = users.find(user => user.id === Number(req.params.userId));
+        res.json(user);
+    }
+    catch (e) {
+        res.status(500).json(e.message);
+    }
+});
 // Deleting user by id
+app.delete('/users/:userId', async (req, res) => {
+    try {
+        const users = await read();
+        const index = users.findIndex(user => user.id === Number(req.params.userId));
+        if (index === -1) {
+            return res.status(404).json('User not found')
+        }
+        users.splice(index, 1);
+        await write(users);
+        res.sendStatus(204);
+    }
+    catch (e) {
+        res.status(500).json(e.message);
+    }
+});
 
 const port = process.env.PORT;
 app.listen(port, () => {
