5458642a7dfcd0d70ad934eb1550540de487b120
david-kasprak <kasprakdavid@gmail.com>
2024-12-12 23:35:39 +0200

    HW-4 (still preparing...)

--- src/main.ts
+++ src/main.ts
@@ -1,111 +1,126 @@
-import express, {Request, Response} from 'express'
-import dotenv  from 'dotenv'
-import {read, write} from './fs.service'
-dotenv.config()
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+
+import { read, write } from "./fs.service";
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Reading users
-app.get('/users', async (req, res): Promise<void> => {
-    try {
-        const users = await read();
-        res.json(users);
-    }
-    catch (e) {
-        res.status(500).json(e.message);
-    }
+app.get("/users", async (req, res): Promise<void> => {
+  try {
+    const users = await read();
+    res.json(users);
+  } catch (e) {
+    res.status(500).json(e.message);
+  }
 });
 // Creating new user
-// @ts-ignore
-app.post('/users', async (req: Request, res: Response): Promise<void> => {
-    try {
-        if (!req.body.name || req.body.name.length < 3) {
-            return res.status(400).json('Name is required and should be minimum 3 symbols');
-        }
-        if (!req.body.email || !req.body.email.includes('@')) {
-            return res.status(400).json('Email is required');
-        }
-        if (!req.body.password || req.body.password.length < 8) {
-            return res.status(400).json('Password is required and should be minimum 8 symbols');
-        }
-        const users = await read();
-        const newUser = {
-            id: users.length ? users [users.length - 1].id + 1 : 1,
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        }
-        users.push(newUser);
-        await write(users);
-        res.status(201).json(newUser);
+app.post("/users", async (req: Request, res: Response): Promise<void> => {
+  try {
+    if (!req.body.name || req.body.name.length < 3) {
+      return res
+        .status(400)
+        .json("Name is required and should be minimum 3 symbols");
     }
-    catch (e) {
-        res.status(500).json(e.message);
+    if (!req.body.email || !req.body.email.includes("@")) {
+      return res.status(400).json("Email is required");
     }
+    if (!req.body.password || req.body.password.length < 8) {
+      return res
+        .status(400)
+        .json("Password is required and should be minimum 8 symbols");
+    }
+    const users = await read();
+    const newUser = {
+      id: users.length ? users[users.length - 1].id + 1 : 1,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    };
+    users.push(newUser);
+    await write(users);
+    res.status(201).json(newUser);
+  } catch (e) {
+    res.status(500).json(e.message);
+  }
 });
 // Reading user by id
-app.get('/users/:userId', async (req: Request, res: Response): Promise<void> => {
+app.get(
+  "/users/:userId",
+  async (req: Request, res: Response): Promise<void> => {
     try {
-        const users = await read();
-        const user = users.find(user => user.id === Number(req.params.userId));
-        res.json(user);
+      const users = await read();
+      const user = users.find((user) => user.id === Number(req.params.userId));
+      res.json(user);
+    } catch (e) {
+      res.status(500).json(e.message);
     }
-    catch (e) {
-        res.status(500).json(e.message);
-    }
-});
+  },
+);
 // Updating user by id
-// @ts-ignore
-app.put('/users/:userId', async (req: Request, res: Response): Promise<void> => {
+app.put(
+  "/users/:userId",
+  async (req: Request, res: Response): Promise<void> => {
     try {
-        if (!req.body.name || req.body.name.length < 3) {
-            return res.status(400).json('Name is required and should be minimum 3 symbols');
-        }
-        if (!req.body.email || !req.body.email.includes('@')) {
-            return res.status(400).json('Email is required');
-        }
-        if (!req.body.password || req.body.password.length < 8) {
-            return res.status(400).json('Password is required and should be minimum 8 symbols');
-        }
-        const users = await read();
-        const index = users.findIndex(user => user.id === Number(req.params.userId));
-        if (index === -1) {
-            return res.status(404).json('User not found')
-        }
-        const user = users[index];
-        user.name = req.body.name;
-        user.email = req.body.email;
-        user.password = req.body.password;
+      if (!req.body.name || req.body.name.length < 3) {
+        return res
+          .status(400)
+          .json("Name is required and should be minimum 3 symbols");
+      }
+      if (!req.body.email || !req.body.email.includes("@")) {
+        return res.status(400).json("Email is required");
+      }
+      if (!req.body.password || req.body.password.length < 8) {
+        return res
+          .status(400)
+          .json("Password is required and should be minimum 8 symbols");
+      }
+      const users = await read();
+      const index = users.findIndex(
+        (user) => user.id === Number(req.params.userId),
+      );
+      if (index === -1) {
+        return res.status(404).json("User not found");
+      }
+      const user = users[index];
+      user.name = req.body.name;
+      user.email = req.body.email;
+      user.password = req.body.password;
 
-        await write(users);
-        res.status(201).json(user);
+      await write(users);
+      res.status(201).json(user);
+    } catch (e) {
+      res.status(500).json(e.message);
     }
-    catch (e) {
-        res.status(500).json(e.message);
-    }
-});
+  },
+);
 // Deleting user by id
-// @ts-ignore
-app.delete('/users/:userId', async (req: Request, res: Response): Promise<void> => {
+app.delete(
+  "/users/:userId",
+  async (req: Request, res: Response): Promise<void> => {
     try {
-        const users = await read();
-        const index = users.findIndex(user => user.id === Number(req.params.userId));
-        if (index === -1) {
-            return res.status(404).json('User not found')
-        }
-        users.splice(index, 1);
-        await write(users);
-        res.sendStatus(204);
+      const users = await read();
+      const index = users.findIndex(
+        (user) => user.id === Number(req.params.userId),
+      );
+      if (index === -1) {
+        return res.status(404).json("User not found");
+      }
+      users.splice(index, 1);
+      await write(users);
+      res.sendStatus(204);
+    } catch (e) {
+      res.status(500).json(e.message);
     }
-    catch (e) {
-        res.status(500).json(e.message);
-    }
-});
+  },
+);
 
 const port = process.env.PORT;
 app.listen(port, () => {
-    console.log(`Server has been started on port ${port}`);
+  console.log(`Server has been started on port ${port}`);
 });
