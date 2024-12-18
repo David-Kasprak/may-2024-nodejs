db006841b9ba694a5a64639594e6a0260c1caf1f
david-kasprak <kasprakdavid@gmail.com>
2024-12-18 16:53:02 +0200

    HW-5 (in progress)

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,12 +1,16 @@
 import { ApiError } from "../errors/api.error";
-import { IUser, IUserDto } from "../interfaces/user.interface";
+import {
+  IUser,
+  IUserCreateDto,
+  IUserUpdateDto,
+} from "../interfaces/user.interface";
 import { userRepository } from "../repositories/user.repository";
 
 class UserService {
   public async getList(): Promise<IUser[]> {
     return await userRepository.getList();
   }
-  public async create(dto: IUserDto): Promise<IUser> {
+  public async create(dto: IUserCreateDto): Promise<IUser> {
     if (!dto.name || dto.name.length < 3) {
       throw new ApiError(
         "Name is required and should be minimum 3 symbols",
@@ -24,37 +28,21 @@ class UserService {
     }
     return await userRepository.create(dto);
   }
-  public async getUserById(userId: number): Promise<IUser> {
+  public async getUserById(userId: string): Promise<IUser> {
     const user = await userRepository.getById(userId);
     if (!user) {
       throw new ApiError("User not found", 404);
     }
     return user;
   }
-  public async updateUser(userId: number, dto: IUserDto): Promise<IUser> {
-    if (!dto.name || dto.name.length < 3) {
-      throw new ApiError(
-        "Name is required and should be minimum 3 symbols",
-        400,
-      );
-    }
-    if (!dto.email || !dto.email.includes("@")) {
-      throw new ApiError("Email is required", 400);
-    }
-    if (!dto.password || dto.password.length < 8) {
-      throw new ApiError(
-        "Password is required and should be minimum 8 symbols",
-        400,
-      );
-    }
-
+  public async updateUser(userId: string, dto: IUserUpdateDto): Promise<IUser> {
     const user = await userRepository.getById(userId);
     if (!user) {
       throw new ApiError("User not found", 404);
     }
     return await userRepository.updateById(userId, dto);
   }
-  public async deleteUser(userId: number): Promise<void> {
+  public async deleteUser(userId: string): Promise<void> {
     const user = await userRepository.getById(userId);
     if (!user) {
       throw new ApiError("User not found", 404);
