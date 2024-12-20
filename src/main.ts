55c4ea638d675c21e2ba4d8d55b1e79229f1cee3
david-kasprak <kasprakdavid@gmail.com>
2024-12-20 11:51:58 +0200

    HW-6 (preparing)

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,19 @@
-import dotenv from "dotenv";
 import express, { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
 
 import { config } from "./configs/config";
 import { ApiError } from "./errors/api.error";
+import { authRouter } from "./routers/auth.router";
 import { userRouter } from "./routers/user.router";
-
-dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/auth", authRouter);
 app.use("/users", userRouter);
 
-// Handling errors
 app.use(
   "*",
   (error: ApiError, req: Request, res: Response, next: NextFunction) => {
@@ -26,7 +24,8 @@ app.use(
   },
 );
 process.on("uncaughtException", (error) => {
-  console.error("Uncaught Exception: ", error);
+  console.error("Uncaught Exception:", error);
+  process.exit(1);
 });
 
 app.listen(config.port, async () => {
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,60 +1,53 @@
 import { ApiError } from "../errors/api.error";
-import {
-  IUser,
-  IUserCreateDto,
-  IUserUpdateDto,
-} from "../interfaces/user.interface";
+import { ITokenPayload } from "../interfaces/token.interface";
+import { IUser, IUserUpdateDto } from "../interfaces/user.interface";
 import { userRepository } from "../repositories/user.repository";
 
 class UserService {
   public async getList(): Promise<IUser[]> {
     return await userRepository.getList();
   }
-  public async create(dto: IUserCreateDto): Promise<IUser> {
-    await this.isEmailUnique(dto.email);
-    if (!dto.name || dto.name.length < 3) {
-      throw new ApiError(
-        "Name is required and should be minimum 3 symbols",
-        400,
-      );
+
+  public async getMe(tokenPayload: ITokenPayload): Promise<IUser> {
+    const user = await userRepository.getById(tokenPayload.userId);
+    if (!user) {
+      throw new ApiError("User not found", 404);
     }
-    if (!dto.email || !dto.email.includes("@")) {
-      throw new ApiError("Email is required", 400);
-    }
-    if (!dto.password || dto.password.length < 8) {
-      throw new ApiError(
-        "Password is required and should be minimum 8 symbols",
-        400,
-      );
-    }
-    return await userRepository.create(dto);
+    return user;
   }
+
+  public async updateMe(
+    tokenPayload: ITokenPayload,
+    dto: IUserUpdateDto,
+  ): Promise<IUser> {
+    const user = await userRepository.getById(tokenPayload.userId);
+    if (!user) {
+      throw new ApiError("User not found", 404);
+    }
+    return await userRepository.updateById(user._id, dto);
+  }
+
+  public async deleteMe(tokenPayload: ITokenPayload): Promise<void> {
+    const user = await userRepository.getById(tokenPayload.userId);
+    if (!user) {
+      throw new ApiError("User not found", 404);
+    }
+    await userRepository.deleteById(tokenPayload.userId);
+  }
+
+  public async isEmailUnique(email: string): Promise<void> {
+    const user = await userRepository.getByEmail(email);
+    if (user) {
+      throw new ApiError("Email is already in use", 409);
+    }
+  }
+
   public async getUserById(userId: string): Promise<IUser> {
     const user = await userRepository.getById(userId);
     if (!user) {
       throw new ApiError("User not found", 404);
     }
     return user;
-  }
-  public async updateUser(userId: string, dto: IUserUpdateDto): Promise<IUser> {
-    const user = await userRepository.getById(userId);
-    if (!user) {
-      throw new ApiError("User not found", 404);
-    }
-    return await userRepository.updateById(userId, dto);
-  }
-  public async deleteUser(userId: string): Promise<void> {
-    const user = await userRepository.getById(userId);
-    if (!user) {
-      throw new ApiError("User not found", 404);
-    }
-    await userRepository.deleteById(userId);
-  }
-  private async isEmailUnique(email: string): Promise<void> {
-    const user = await userRepository.getByEmail(email);
-    if (user) {
-      throw new ApiError("Email is already in use", 409);
-    }
   }
 }
 
